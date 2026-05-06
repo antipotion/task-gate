dda538a9704f112717bf35df55d96be6685eac97
antipotion <hello.antipotion@gmail.com>
2026-05-06 18:58:16 +0800

    refactor: simplified task category component to 'TaskSort' instead of dedicated 'TaskTodo', 'TaskInProgress', 'TaskCompleted'.
    - added filtering capability to 'TaskSort' to represent different task
      status

--- src/app/project/project-dashboard/project-tasks-board/project-tasks-board.ts
+++ src/app/project/project-dashboard/project-tasks-board/project-tasks-board.ts
@@ -1,13 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { TaskTodo } from "../../task/task-todo/task-todo";
-import { TaskInProgress } from "../../task/task-in-progress/task-in-progress";
-import { TaskCompleted } from "../../task/task-completed/task-completed";
 import { ActivatedRoute } from '@angular/router';
+import { TaskSort } from '../../task/task-sort/task-sort';
 
 @Component({
   selector: 'app-project-tasks-board',
-  imports: [MatIconModule, TaskTodo, TaskInProgress, TaskCompleted],
+  imports: [MatIconModule, TaskSort],
   templateUrl: './project-tasks-board.html',
   styleUrl: './project-tasks-board.scss',
 })
--- src/app/project/task/task-sort/task-sort.ts
+++ src/app/project/task/task-sort/task-sort.ts
@@ -3,19 +3,20 @@ import { MatCardModule } from '@angular/material/card';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { Router } from '@angular/router';
 import { TaskFacade, TaskState } from '../task-facade';
-import type { Task } from '../task.model';
+import type { Task, TaskStatus } from '../task.model';
 
 @Component({
-  selector: 'app-task-todo',
+  selector: 'app-task-sort',
   imports: [MatProgressSpinnerModule, MatCardModule],
-  templateUrl: './task-todo.html',
-  styleUrl: './task-todo.scss',
+  templateUrl: './task-sort.html',
+  styleUrl: './task-sort.scss',
 })
-export class TaskTodo {
+export class TaskSort {
   private taskFacade = inject(TaskFacade);
   private router = inject(Router);
 
   readonly projectId = input.required<string>();
+  readonly projectStatus = input.required<TaskStatus>();
 
   taskState = computed<TaskState>(() => this.taskFacade.taskState());
   todoTask = computed<Task[] | null>(() => {
@@ -23,7 +24,7 @@ export class TaskTodo {
     if (state.status !== 'success') return null;
 
     return state.data.filter(
-      (task) => task.projectId === this.projectId() && task.status === 'TODO',
+      (task) => task.projectId === this.projectId() && task.status === this.projectStatus(),
     );
   });
 
