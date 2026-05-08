672d9e33cb6e3a3f45bd61d27adb9b755c8a8715
antipotion <hello.antipotion@gmail.com>
2026-05-08 19:02:18 +0800

    refactor: enforced stricter architectural boundaries for AuthFacade
    - AuthFacade now focuses on UI concerns and orchestration
    - created a dedicated SignupSessionStorage for handling localStorage

--- src/app/application/store/store-service.ts
+++ src/app/application/store/store-service.ts
@@ -15,24 +15,24 @@ export class StoreService {
     .listenToProjects$()
     .pipe(shareReplay({ bufferSize: 1, refCount: true }));
 
+  readonly tasks$: Observable<Task[]> = this._repo
+    .listenToTasks$()
+    .pipe(shareReplay({ bufferSize: 1, refCount: true }));
+
+  readonly teams$: Observable<TeamModel[]> = this._repo
+    .listenToTeams$()
+    .pipe(shareReplay({ bufferSize: 1, refCount: true }));
+
   getProjectById$(projectId: string): Observable<Project | undefined> {
     return this.projects$.pipe(
       map((projects) => projects.find((project) => project.id === projectId)),
     );
   }
 
-  readonly tasks$: Observable<Task[]> = this._repo
-    .listenToTasks$()
-    .pipe(shareReplay({ bufferSize: 1, refCount: true }));
-
   getTaskById$(taskId: string): Observable<Task | undefined> {
     return this.tasks$.pipe(map((tasks) => tasks.find((task) => task.id === taskId)));
   }
   
-  readonly teams$: Observable<TeamModel[]> = this._repo
-    .listenToTeams$()
-    .pipe(shareReplay({ bufferSize: 1, refCount: true }));
-
   getTeamById(teamId: string): Observable<TeamModel | undefined> {
     return this.teams$.pipe(map((teams) => teams.find((team) => team.id === teamId)));
   }
--- src/app/authentication/auth-facade.ts
+++ src/app/authentication/auth-facade.ts
@@ -4,11 +4,13 @@ import { AuthUseCase } from './auth-use-case';
 import { TeamModel } from './sign-up/team/team.model';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { StoreService } from '../application/store/store-service';
+import { SignupSessionStorage } from '../infrastructure/signup-session-storage/signup-session-storage';
 
 @Injectable({ providedIn: 'root' })
 export class AuthFacade {
   private readonly _authUseCase = inject(AuthUseCase);
   private readonly _store = inject(StoreService);
+  private readonly _signupSessionStorage = inject(SignupSessionStorage);
 
   readonly userRole = signal<RoleModel | null>(null);
   readonly userTeamId = computed<string | null>(() => {
@@ -16,6 +18,8 @@ export class AuthFacade {
   });
   readonly team = signal<TeamModel | null>(null);
   readonly user = signal<UserModel | null>(null);
+
+  readonly teamsCollection = toSignal(this._store.teams$, { initialValue: null });
 
   loginWithEmailAndPassword(email: string, password: string): void {
     this._authUseCase.login(email, password);
@@ -26,18 +30,17 @@ export class AuthFacade {
   }
 
   async signUpWithEmailAndPassword(email: string, password: string): Promise<void> {
-    const teamIdFromLocalStorage = localStorage.getItem('teamId');
-    const userRoleFromLocalStorage = localStorage.getItem('userRole');
-    const userRole: RoleModel | null = this.userRole() ?? JSON.parse(userRoleFromLocalStorage ?? '') ?? null;
-    const userTeamId: string | null = this.userTeamId() ?? JSON.parse(teamIdFromLocalStorage ?? '') ?? null;
+    const userRole: RoleModel | null = this.userRole() ?? this._signupSessionStorage.getUserRole();
+    const userTeamId: string | null = this.userTeamId() ?? this._signupSessionStorage.getTeamId();
+    
     if (!userRole || !userTeamId) {
       console.error('No userRole or userTeamId');
       return;
     }
 
     const userData = await this._authUseCase.register(email, password, userRole, userTeamId);
+    
     this.user.set(userData);
-    console.log(this.user());
   }
 
   setUserRole(role: RoleModel): void {
@@ -61,11 +64,16 @@ export class AuthFacade {
     this.team.set(result);
   }
 
-  readonly teams = toSignal(this._store.teams$, { initialValue: null });
-
   getTeamById(teamId: string): TeamModel | null {
-    const result = this.teams()?.find((teams) => teams.id === teamId) ?? null;
-    localStorage.setItem('teamId', JSON.stringify(result?.id));
+    const result = this.teamsCollection()?.find((teams) => teams.id === teamId);
+    
+    if (!result) {
+      // TODO: Show this error to the UI
+      console.error('No team with such teamId is found');
+      return null;
+    }
+    
+    this._signupSessionStorage.saveTeamId(result.id)
 
     return result;
   }
