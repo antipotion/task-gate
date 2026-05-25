21b69e563b17cab4b2379b75159e7d62528819b4
antipotion <hello.antipotion@gmail.com>
2026-05-26 05:11:14 +0800

    update(task-overview): add task description
    - add loading state

--- src/app/project/task/task-overview/task-overview.ts
+++ src/app/project/task/task-overview/task-overview.ts
@@ -2,13 +2,15 @@ import { DatePipe } from '@angular/common';
 import { Component, input } from '@angular/core';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-task-overview',
-  imports: [MatIconModule, MatChipsModule, DatePipe],
+  imports: [MatIconModule, MatChipsModule, DatePipe, MatProgressSpinnerModule],
   templateUrl: './task-overview.html',
   styleUrl: './task-overview.scss',
 })
 export class TaskOverview {
-  readonly taskDeadline = input.required<string>();
+  readonly taskDeadline = input.required<string | null>();
+  readonly taskDescription = input.required<string | null>();
 }
