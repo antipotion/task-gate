d5134831e109e65f61a9dbd3d98849bc7025f0a8
antipotion <hello.antipotion@gmail.com>
2026-05-26 05:04:08 +0800

    update(project-overview): add project description visibility to UI
    - add loading states to project description and deadline

--- src/app/project/project-dashboard/project-overview/project-overview.ts
+++ src/app/project/project-dashboard/project-overview/project-overview.ts
@@ -1,13 +1,15 @@
 import { Component, input } from '@angular/core';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-project-overview',
-  imports: [MatChipsModule, MatIconModule],
+  imports: [MatChipsModule, MatIconModule, MatProgressSpinnerModule],
   templateUrl: './project-overview.html',
   styleUrl: './project-overview.scss',
 })
 export class ProjectOverview {
-  readonly deadline = input.required<string>();
+  readonly deadline = input.required<string | null>();
+  readonly description = input.required<string | null>();
 }
