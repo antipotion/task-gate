2d568d5c288c058afddc307ade20ff59de869198
antipotion <hello.antipotion@gmail.com>
2026-06-02 14:46:56 +0800

    feat(task-status): add task status to 'task-header'

--- src/app/project/task/task-header/task-header.ts
+++ src/app/project/task/task-header/task-header.ts
@@ -1,12 +1,14 @@
+import { NgClass, TitleCasePipe } from '@angular/common';
 import { Component, input } from '@angular/core';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-task-header',
-  imports: [MatProgressSpinnerModule],
+  imports: [MatProgressSpinnerModule, TitleCasePipe, NgClass],
   templateUrl: './task-header.html',
   styleUrl: './task-header.scss',
 })
 export class TaskHeader {
   readonly taskName = input.required<string | undefined>();
+  readonly taskStatus = input.required<string | undefined>();
 }
