57c5587fdc705b601805d92ced5375e6f7083f7c
antipotion <hello.antipotion@gmail.com>
2026-06-05 16:25:22 +0800

    feat(url-submission): add url submission for 'task-action'
    - add duplicate validation on submit for 'task-action'

--- src/app/project/task/task-action/task-action.ts
+++ src/app/project/task/task-action/task-action.ts
@@ -1,8 +1,11 @@
 import { TitleCasePipe } from '@angular/common';
-import { Component, ElementRef, input, output, signal, viewChild } from '@angular/core';
+import { Component, input, output, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatChipsModule } from '@angular/material/chips';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { TaskActionModel, type TaskStatus as TaskStatusModel } from '../task.model';
 
@@ -14,18 +17,23 @@ import { TaskActionModel, type TaskStatus as TaskStatusModel } from '../task.mod
     MatButtonModule,
     TitleCasePipe,
     MatProgressSpinnerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './task-action.html',
   styleUrl: './task-action.scss',
 })
 export class TaskAction {
-  private readonly _fileInput = viewChild<ElementRef<HTMLInputElement>>('file-input');
-
   readonly taskStatus = input.required<TaskStatusModel | undefined>();
   readonly taskNextAction = input.required<TaskActionModel | null>();
   readonly nextActionTriggered = output<TaskActionModel>();
 
-  readonly selectedFileName = signal<string | null>(null);
+  readonly urlLinkSubmitted = signal<string[]>([]);
+
+  readonly urlForm = new FormGroup({
+    urlLink: new FormControl('', [Validators.required, Validators.pattern(/^https?:\/\/.+/)]),
+  });
 
   onNextActionTrigger(): void {
     const taskNextAction = this.taskNextAction();
@@ -34,23 +42,31 @@ export class TaskAction {
     this.nextActionTriggered.emit(taskNextAction);
   }
 
-  onFileSelected(event: Event): void {
-    const input = event.target as HTMLInputElement;
+  onSubmitUrl(): void {
+    const urlLinkControl = this.urlForm.controls.urlLink;
+    if (!urlLinkControl || urlLinkControl.invalid) return;
 
-    if (!input.files?.length) {
+    const urlLink = urlLinkControl.value;
+    if (!urlLink) return;
+
+    if (this.urlLinkSubmitted().includes(urlLink)) {
+      urlLinkControl.setErrors({ duplicate: true });
       return;
     }
 
-    const file = input.files[0];
-    this.selectedFileName.set(file.name);
+    this.urlLinkSubmitted.update((urls) => [...urls, urlLink]);
+
+    this.urlForm.reset();
   }
 
-  onRemoveFile(): void {
-    const fileInput = this._fileInput();
+  onRemoveUrl(url: string): void {
+    this.urlLinkSubmitted.update((urls) => urls.filter((urlStored) => urlStored !== url));
+  }
 
-    this.selectedFileName.set(null);
+  onSubmitProgress(): void {
+    const urlLinks = this.urlLinkSubmitted();
+    if (!urlLinks || urlLinks.length === 0) return;
 
-    if (!fileInput) return;
-    fileInput.nativeElement.value = '';
+    this.onNextActionTrigger();
   }
 }
