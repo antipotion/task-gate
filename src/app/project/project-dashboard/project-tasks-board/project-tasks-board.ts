6d7aac551a845c043a66e81e841a9f717adafe61
antipotion <hello.antipotion@gmail.com>
2026-06-17 06:16:25 +0800

    refactor(project-details): redesign UI for task board

--- src/app/project/project-dashboard/project-tasks-board/project-tasks-board.ts
+++ src/app/project/project-dashboard/project-tasks-board/project-tasks-board.ts
@@ -1,17 +1,41 @@
-import { Component, inject } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
+import { Component, effect, inject, Signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { ActivatedRoute } from '@angular/router';
-import { TaskSort } from '../../task/task-sort/task-sort';
+import { map } from 'rxjs';
+import { StoreService } from '../../../application/store/store-service';
 import { PROJECT_ROUTE_PARAMS } from '../../project.routes';
+import { TaskStatus } from '../../task/task.model';
+import { ProjectTaskBoardCard } from '../project-task-board-card/project-task-board-card';
 
 @Component({
   selector: 'app-project-tasks-board',
-  imports: [MatIconModule, TaskSort],
+  imports: [ProjectTaskBoardCard],
   templateUrl: './project-tasks-board.html',
   styleUrl: './project-tasks-board.scss',
 })
 export class ProjectTasksBoard {
   private readonly _route = inject(ActivatedRoute);
+  private readonly _storeService = inject(StoreService);
 
-  readonly projectId = this._route.snapshot.paramMap.get(PROJECT_ROUTE_PARAMS.projectId) || '';
+  readonly taskCategories: TaskStatus[] = [
+    'TODO',
+    'IN-PROGRESS',
+    'REVIEWING',
+    'REJECTED',
+    'APPROVED',
+  ];
+
+  readonly projectId: Signal<string | null> = toSignal(
+    this._route.paramMap.pipe(map((params) => params.get(PROJECT_ROUTE_PARAMS.projectId))),
+    { initialValue: null },
+  );
+
+  constructor() {
+    effect(() => {
+      const projectId = this.projectId();
+      if (!projectId) return;
+
+      this._storeService.setProjectId(projectId);
+    });
+  }
 }
