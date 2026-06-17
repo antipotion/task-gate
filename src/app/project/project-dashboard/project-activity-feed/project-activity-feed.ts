422c44d34452582abbcb915de6eafb33f8c2a3bc
antipotion <hello.antipotion@gmail.com>
2026-06-18 02:32:43 +0800

    remove(project-activitiy-feed): will not implement in the near future

--- src/app/project/project-dashboard/project-activity-feed/project-activity-feed.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-project-activity-feed',
-  imports: [],
-  templateUrl: './project-activity-feed.html',
-  styleUrl: './project-activity-feed.scss',
-})
-export class ProjectActivityFeed {}
