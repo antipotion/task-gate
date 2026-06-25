2c79339f4e36f441f076e80bf914daba5739abad
antipotion <hello.antipotion@gmail.com>
2026-06-26 05:03:14 +0800

    refactor(login): fix issue of login
    - no error happens on login now

--- src/app/authentication/login/login.ts
+++ src/app/authentication/login/login.ts
@@ -1,5 +1,12 @@
-import { AfterViewInit, Component, inject, OnDestroy, signal } from '@angular/core';
-import { toObservable } from '@angular/core/rxjs-interop';
+import {
+  AfterViewInit,
+  Component,
+  computed,
+  effect,
+  inject,
+  OnDestroy,
+  signal,
+} from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
@@ -7,7 +14,6 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { ActivatedRoute, Router } from '@angular/router';
-import { filter, firstValueFrom } from 'rxjs';
 import { ROUTES_PARAMS } from '../../app.routes';
 import { Disclaimer } from '../../disclaimer/disclaimer';
 import { AuthFacade } from '../auth-facade';
@@ -33,7 +39,7 @@ export class Login implements AfterViewInit, OnDestroy {
   private readonly _dialog = inject(MatDialog);
 
   readonly loggingInLoading = signal<boolean>(false);
-  readonly isAuthenticated$ = toObservable(this._authFacade.isAuthenticated);
+  readonly isAuthenticated = computed(() => this._authFacade.isAuthenticated());
 
   loginForm = new FormGroup({
     email: new FormControl('', {
@@ -53,17 +59,21 @@ export class Login implements AfterViewInit, OnDestroy {
     this.openDisclaimerDialog();
   }
 
+  constructor() {
+    effect(() => {
+      const isAuthenticated = this.isAuthenticated();
+      if (!isAuthenticated) return;
+
+      this._router.navigate([ROUTES_PARAMS.project]);
+    });
+  }
+
   async loginWithEmailAndPassword(): Promise<void> {
     const email: string = this.emailControl.getRawValue();
     const password: string = this.passwordControl.getRawValue();
 
     this.loggingInLoading.set(true);
     await this._authFacade.loginWithEmailAndPassword(email, password);
-
-    // Only truthy value passes the filter(Boolean)
-    await firstValueFrom(this.isAuthenticated$.pipe(filter(Boolean)));
-
-    this._router.navigate([ROUTES_PARAMS.project]);
   }
 
   loginWithGoogle(): void {
